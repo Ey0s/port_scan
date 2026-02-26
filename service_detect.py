cff11dbde0107e44875ce7f1ca0ee8dd97c39cac
Ey0s <eyosyasyosef1934@gmail.com>
2026-02-26 20:39:53 +0300

    port scan service detection

---
+++ service_detect.py
@@ -0,0 +1,17 @@
+COMMON_PORTS = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3306: "MySQL",
+    5432: "PostgreSQL",
+}
+
+
+def detect_service(port):
+    return COMMON_PORTS.get(port, "Unknown Service")
