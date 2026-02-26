63ff97735dfb697e61080031fe3d3336e5e87b58
Ey0s <eyosyasyosef1934@gmail.com>
2026-02-26 20:34:22 +0300

    port scan scanner file

---
+++ scanner.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+class TCP_scanner:
+    def __init__(self,target,timeout=1):
+        self.target=target
+        self.timeout=timeout
+        self.open_ports=[]
+        self.lock=threading.Lock()
+    def scan_port(self,port):
+        try:
+            sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            sock.settimeout(self.timeout)
+            result=sock.connect_ex((self.target,port))
+            if result==0:
+                with self.lock:
+                    self.open_ports.append(port)
+            sock.close()
+
+        except sock.error:
+            pass
+    def run_scan(self, start_port, end_port):
+        threads = []
+
+        for port in range(start_port, end_port + 1):
+            thread = threading.Thread(target=self.scan_port, args=(port,))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        return sorted(self.open_ports)
